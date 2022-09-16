dcf2ec0f7e68c0233eedf3e5e46bb2f49c5b309d
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2022-09-16 11:48:09 -0500

    Some changes
    Sync and edits

--- func/Examen01_01.R
+++ func/Examen01_01.R
@@ -1,27 +1,14 @@
-#
-# 
-library(tidyverse)
-
-HERS <- read_csv("https://raw.githubusercontent.com/fabarrios/Regression/master/DataRegressBook/Chap3/hersdata.csv", show_col_types = FALSE)
-# Loading the HERS database in HERS variable
-names(HERS)
-
-HERS
-summary(HERS)
-
-HERS_01 <- HERS %>% select(BMI, diabetes, glucose)
-HERS_01 <- HERS_01 %>% mutate(gluc_high = glucose > 126)
-HERS_01 <- HERS_01 %>% mutate(gluc_pre = glucose >= 100 & glucose <= 126)
-HERS_01 <- HERS_01 %>% mutate(gluc_norm = glucose < 100)
-
 # Exam01 LicTec
 # 12-sep-2022
+library(tidyverse)
+
 NCBIRTH800 <- read_csv("~/Dropbox/GitHub/ProbEstad/DataSets/ch02_all/LDS_C02_NCBIRTH800.csv", show_col_types = FALSE)
 NCBIRTH_sht <- NCBIRTH800 %>% select(mage, weeks, gained, tounces, tgrams)
 
 summary(NCBIRTH_sht)
 sd(NCBIRTH_sht$mage, na.rm = TRUE)
 sd(NCBIRTH_sht$weeks, na.rm = TRUE)
+sd(NCBIRTH_sht$gained, na.rm = TRUE)
 sd(NCBIRTH_sht$tounces, na.rm = TRUE)
 sd(NCBIRTH_sht$tgrams, na.rm = TRUE)
 
@@ -56,6 +43,22 @@ boxplot(NCBIRTH_sht$weeks, col = 4)
 boxplot(NCBIRTH_sht$gained, col = 4)
 boxplot(NCBIRTH_sht$tgrams, col = 4)
 
+NCBIRTH_sht %>% drop_na(mage) %>%
+  ggplot(aes(x= "", y= mage, color = mage)) +
+  geom_boxplot() +
+  geom_jitter(alpha = 0.4, position = position_jitter(0.15)) +
+  theme_bw()
+
+NCBIRTH_sht %>% drop_na(gained) %>%
+  ggplot(aes(x= "", y= gained, color = gained)) +
+  geom_boxplot(color = 2) +
+  geom_jitter(alpha = 0.4, position = position_jitter(0.15)) +
+  theme_bw()
+
+# layout(1)
+# boxplot(tgrams ~ smoke, data = NCBIRTH800)
+# boxplot(mage ~ marital, data = NCBIRTH800)
+ 
 NCBIRTH800 %>% drop_na(smoke) %>%
   ggplot(aes(x= factor(smoke), y= tgrams, color= factor(smoke))) +
   geom_boxplot() +
@@ -65,5 +68,5 @@ NCBIRTH800 %>% drop_na(smoke) %>%
 NCBIRTH800 %>% drop_na(marital) %>%
   ggplot(aes(x= factor(marital), y= mage, color= factor(marital))) +
   geom_boxplot() +
-  geom_jitter(alpha= 0.25, position = position_jitter(0.15)) +
+  geom_jitter(alpha = 0.25, position = position_jitter(0.15)) +
   theme_bw()
